0971c5fbcb6612e4f2807fd184487638a2672662
spietari18 <spietari.18@gmail.com>
2022-04-21 10:11:59 +0300

    Fixed gcc warnings and errors, cd and path seems to be working.

--- wish.c
+++ wish.c
@@ -25,39 +25,51 @@ int main(int argc, char * * argv) {
     printf("wish> ");
     
 
-    while (getline(&buffer,&bufsize, stdin) != NULL) {
+    while (getline(&buffer,&bufsize, stdin) != -1) {
         char s2[160];
-        strncpy(s2, buffer, sizeof(buffer));
-        char s1 = strtok(s2, "\000");
+        strncpy(s2, buffer, bufsize * sizeof(char));
+        char *s1;
+        s1 = (char *)malloc(bufsize * sizeof(char));
+        if (s1 == NULL)
+        {
+            perror("Unable to allocate buffer");
+            exit(1);
+        }
+        s1 = strtok(s2, " ");
         
-        
-        if (strcmp(s2, "exit\n") == 0) {
+        if (strcmp(s1, "exit\n") == 0) {
             exit(0);
             return 0;
-        }else if (strcmp(s2, "cd")== 0){
-            //printf(s1);
-            chdir(s1);
-            system("ls");
-        }else if(strcmp(s2,"path\n")){
+        } else if (strcmp(s1, "cd") == 0){
+            s1 = strtok(NULL, "\n");
+            //printf("%s\n", s1);
+            if (chdir(s1) == -1) {
+                perror("chdir failed");
+                printf("%s\n", s1);
+                exit(1);
+            }
+            //system("ls");
+            printf("wish> ");
+        } else if (strcmp(s1,"path\n") == 0){
             char cwd[1024];
             getcwd(cwd,sizeof(cwd));
-            printf("%s",cwd);
+            printf("%s\nwish> ",cwd);
 
-        }
+        } else {
+            switch(cpid = fork()) {
+                case -1:
+                    die("fork");
+                    break;
 
-        switch(cpid = fork()) {
-            case -1:
-                die("fork");
-                break;
+                case 0:
+                    system(buffer);
+                    exit(0);
+                    break;
 
-            case 0:
-                system(buffer);
-                exit(0);
-                break;
-
-            default:
-                waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
-                printf("wish> ");
+                default:
+                    waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
+                    printf("wish> ");
+            }
         }
     }
     return 0;
